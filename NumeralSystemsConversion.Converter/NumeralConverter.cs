452501d8f853457ce982971f8a02c4008a3a3bc1
calt08 <cesarlopez2808@gmail.com>
2020-07-08 22:25:30 -0400

    Almost ready

--- NumeralSystemsConversion.Converter/NumeralConverter.cs
+++ NumeralSystemsConversion.Converter/NumeralConverter.cs
@@ -5,18 +5,22 @@ namespace NumeralSystemsConversion.Converter
 {
     static public class NumeralConverter
     {
-        static public string IntToRoman(int num){
-            if (num > 3999 || num < 1){
+        static public string IntToRoman(int num)
+        {
+            if (num > 3999 || num < 1)
+            {
                 throw new IndexOutOfRangeException("Roman number needs to be between 1 and 3999");
             }
 
-            int[] nums = {5, 1};
-            string[] romanNums = {"V", "I"};
+            int[] nums = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            string[] romanNums = new string[] { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
             string romanNum = "";
             int x = 0;
-            while(num > 0){
-                for (int i = 0; i < num / nums[x]; i++)
+            while (num > 0)
+            {
+                int limit = num / nums[x];
+                for (int i = 0; i < limit; i++)
                 {
                     romanNum += romanNums[x];
                     num -= nums[x];
--- NumeralSystemsConversion.Tests/UnitTest1.cs
+++ NumeralSystemsConversion.Tests/UnitTest1.cs
@@ -9,27 +9,75 @@ namespace NumeralSystemsConversion.Tests
         [Fact]
         public void Int1ReturnsRomanI()
         {
-            Assert.Equal(NumeralConverter.IntToRoman(1), "I");
+            Assert.Equal("I", NumeralConverter.IntToRoman(1));
         }
 
         [Fact]
-        public void IntOverRomanRange(){
+        public void IntOverRomanRange()
+        {
             Assert.Throws<IndexOutOfRangeException>(() => NumeralConverter.IntToRoman(4000));
-        } 
+        }
 
         [Fact]
-        public void IntUnderRomanRange(){
+        public void IntUnderRomanRange()
+        {
             Assert.Throws<IndexOutOfRangeException>(() => NumeralConverter.IntToRoman(0));
         }
 
         [Fact]
-        public void Int2ReturnsRomanII(){
-            Assert.Equal(NumeralConverter.IntToRoman(2), "II");
+        public void Int2ReturnsRomanII()
+        {
+            Assert.Equal("II", NumeralConverter.IntToRoman(2));
         }
 
         [Fact]
-        public void Int5ReturnsRomanV(){
-            Assert.Equal(NumeralConverter.IntToRoman(5), "V");
+        public void Int5ReturnsRomanV()
+        {
+            Assert.Equal("V", NumeralConverter.IntToRoman(5));
         }
+
+        [Fact]
+        public void Int10ReturnsRomanX()
+        {
+            Assert.Equal("X", NumeralConverter.IntToRoman(10));
+        }
+
+        [Fact]
+        public void Int4ReturnsRomanIV()
+        {
+            Assert.Equal("IV", NumeralConverter.IntToRoman(4));
+        }
+
+        [Fact]
+        public void Int400ReturnsRomanCD()
+        {
+            Assert.Equal("CD", NumeralConverter.IntToRoman(400));
+        }
+
+        [Fact]
+        public void Int48ReturnsRomanXLVIII()
+        {
+            Assert.Equal("XLVIII", NumeralConverter.IntToRoman(48));
+        }
+
+        [Fact]
+        public void Int498ReturnsRomanCDXCVIII()
+        {
+            Assert.Equal("CDXCVIII", NumeralConverter.IntToRoman(498));
+        }
+
+        [Fact]
+        public void Int1021ReturnsRomanMXXI()
+        {
+            Assert.Equal("MXXI", NumeralConverter.IntToRoman(1021));
+        }
+        [Fact]
+        public void Int2020ReturnsRomanMMXX()
+        {
+            Assert.Equal("MMXX", NumeralConverter.IntToRoman(2020));
+        }
+
+
+
     }
 }
